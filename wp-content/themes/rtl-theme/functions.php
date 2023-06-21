73a1e3a5cbd0e4fb45512dda746c1873585e85ed
KiarashAlishahbazpoori <kiarash.shahbazi@gmail.com>
2023-06-21 21:26:03 +0330

    post api : have_posts() function and  show post in loop

--- wp-content/themes/rtl-theme/functions.php
+++ wp-content/themes/rtl-theme/functions.php
@@ -1,27 +1,8 @@
 <?php
 
-global $wpdb;
-$table_name = $wpdb->prefix . 'ks_tickets';
+add_theme_support( 'post-thumbnails' );
 
-/*$title = "title ticket update";
-$content = "content ticket update";
-$id = 1;*/
-/*$wpdb->query(
-	$wpdb->prepare("update {$table_name} set title = %s, content = %s where id = %d", $title, $content, $id)
-);
-*/
-/*$data = ['title' => $title, 'content' => $content];
-$data_format =['%s', '%s'];
-$where = ['id' => $id];
-$where_format = ['%d'];
 
-$wpdb->update($table_name, $data, $where, $data_format, $where_format);*/
-
-$id = 4;
-/*$wpdb->query(
-	$wpdb->prepare("delete from {$table_name} where id = %d", $id)
-);*/
-
-$where = ['id' => $id];
-$where_format = ['%d'];
-$wpdb->delete($table_name, $where, $where_format);
+function rtl_excerpt( $string ) {
+	return substr( $string, 0, 150 );
+}
--- wp-content/themes/rtl-theme/index.php
+++ wp-content/themes/rtl-theme/index.php
@@ -1,1 +1,36 @@
-<?php
+<!doctype html>
+<html lang="fa" dir="rtl">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>مستر کلاس راست چین</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.rtl.min.css"
+          integrity="sha384-PJsj/BTMqILvmcej7ulplguok8ag4xFTPryRq8xevL7eBYSmpXKcbNVuy+P0RMgq" crossorigin="anonymous">
+</head>
+<body dir="rtl">
+<div class="container">
+    <h3>لیست پست ها</h3>
+    <div class="row">
+		<?php if ( have_posts() ): ?>
+			<?php while ( have_posts() ): the_post(); ?>
+                <div class="card" style="width: 18rem;margin: 1rem">
+                    <?php if (has_post_thumbnail()): ?>
+                        <?php the_post_thumbnail('full', ['class'=>'card-img-top', 'alt'=>get_the_title()]); ?>
+                    <?php else: ?>
+                        <img src="https://dkstatics-public.digikala.com/digikala-products/114356296.jpg?x-oss-process=image/resize,m_lfit,h_300,w_300/quality,q_80"
+                         class="card-img-top" alt="...">
+                    <?php endif; ?>
+                    <div class="card-body">
+                        <h5 class="card-title"><?php the_title(); ?></h5>
+                        <p class="card-text"><?php echo rtl_excerpt(get_the_excerpt()) . " ..."; ?></p>
+                        <a href="<?php the_permalink();?>" class="btn btn-primary">مطالعه</a>
+                    </div>
+                </div>
+			<?php endwhile; ?>
+		<?php endif; ?>
+    </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"
+            integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz"
+            crossorigin="anonymous"></script>
+</body>
+</html>
