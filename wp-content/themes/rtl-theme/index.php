560ac1f87d619f2eb83b292a3b3afbe3cf1b67a7
KiarashAlishahbazpoori <kiarash.shahbazi@gmail.com>
2023-06-24 21:25:05 +0330

    post api : WP_QUERY() part1 and show post by filter

--- wp-content/themes/rtl-theme/index.php
+++ wp-content/themes/rtl-theme/index.php
@@ -32,5 +32,38 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"
             integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz"
             crossorigin="anonymous"></script>
+</div>
+<div class="container">
+    <h3>لیست سفارشی پست ها</h3>
+    <div class="row">
+	    <?php
+	    $args  = [
+		    'author' => 1,
+		    'cat'    => 1
+	    ];
+	    $query = new WP_Query( $args );
+	    ?>
+		<?php if ( $query->have_posts() ): ?>
+			<?php while ( $query->have_posts() ): $query->the_post(); ?>
+                <div class="card" style="width: 18rem;margin: 1rem">
+					<?php if (has_post_thumbnail()): ?>
+						<?php the_post_thumbnail('full', ['class'=>'card-img-top', 'alt'=>get_the_title()]); ?>
+					<?php else: ?>
+                        <img src="https://dkstatics-public.digikala.com/digikala-products/114356296.jpg?x-oss-process=image/resize,m_lfit,h_300,w_300/quality,q_80"
+                             class="card-img-top" alt="...">
+					<?php endif; ?>
+                    <div class="card-body">
+                        <h5 class="card-title"><?php the_title(); ?></h5>
+                        <p class="card-text"><?php echo rtl_excerpt(get_the_excerpt()) . " ..."; ?></p>
+                        <a href="<?php the_permalink();?>" class="btn btn-danger">مطالعه</a>
+                    </div>
+                </div>
+			<?php endwhile; ?>
+		<?php endif; ?>
+    </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"
+            integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz"
+            crossorigin="anonymous"></script>
+</div>
 </body>
 </html>
